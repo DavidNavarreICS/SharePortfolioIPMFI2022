c65b33c2139f51ecf6abd45da8e30ede49695aec
David Navarre <David.Navarre@irit.fr>
2023-02-16 11:46:35 +0100

    First commit

---
+++ src/test/java/tp04/metier/ActionSimpleTest.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package tp04.metier;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author David Navarre <David.Navarre@irit.fr>
+ */
+public class ActionSimpleTest {
+
+    public ActionSimpleTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+
+}
